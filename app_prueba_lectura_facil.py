2a524c7803fe04938cb0c325dae26c3657ee0824
islueje <islueje@yahoo.es>
2026-03-15 21:36:56 +0100

    Update app_prueba_lectura_facil.py

--- app_prueba_lectura_facil.py
+++ app_prueba_lectura_facil.py
@@ -34,10 +34,12 @@ import py_lectura_facil as lf
 
 
 
-texto = "En ocasiones; sin embargo; se escribe etcétera o etc. con la puntuación; inapropiada, como en los siguientes ejemplos: «Los inspectores les comunicaron a los titulares de los puestos de fruta, verduras, ropa, calzado, etc... que tendrán que instalarse más arriba» y «Asimismo se ocuparán otros espacios del recinto, como vestuarios, zonas para camerinos etc...»."
+texto = "En ocasiones sin embargo se escribe etcétera o etc. con la puntuación inapropiada, como en los siguientes ejemplos: «Los inspectores les comunicaron a los titulares de los puestos de fruta, verduras, ropa, calzado, etc... que tendrán que instalarse más arriba» y «Asimismo se ocuparán otros espacios del recinto, como vestuarios, zonas para camerinos etc...»."
 
 
 puntuacion, recomendaciones = lf.ValidacionLFTexto(texto)
+
+rango, descripcion_rango = lf.RangoLFPuntuacion(puntuacion)
 
 
 print("")
@@ -45,7 +47,9 @@ print("Texto:\n\n" + texto)
 print("")
 print("Puntuación LF: " + str(puntuacion))
 print("")
+print("Rango LF: " + rango + ", " + descripcion_rango)
+print("")
 print("Recomendaciones:\n\n" + recomendaciones)
-    
-    
 
+    
+    
